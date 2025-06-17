9b9d6657f27e159a4e75c5a01329fd5e57be7270
Atharva <atharvamandle19@gmail.com>
2025-06-17 14:47:02 +0530

    logout done

--- hiring/account/urls.py
+++ hiring/account/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('login/',views.login_view,name='login'),
     path('register/',views.register_view,name='register'),
-    path('activate/<str:uidb64>/<str:token>/',views.activate_account,name='activate')
+    path('activate/<str:uidb64>/<str:token>/',views.activate_account,name='activate'),
+    path('logout/',LogoutView.as_view(next_page='login'),name='logout')
 
 ]
